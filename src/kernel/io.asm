e94bdea405be1e157e539a9a0b73ca2e13abf990
FlowerBlackG <fb2466375479@outlook.com>
2022-07-02 23:57:29 +0800

    添加io操作汇编文件。内核编译makefile的bug修复。改变头文件路径结构。

--- src/kernel/io.asm
+++ src/kernel/io.asm
@@ -7,6 +7,26 @@ section .text ; 代码段。
 
 global io_in_byte
 io_in_byte:
+    ; 保存栈帧。
+    push ebp
+    mov ebp, esp
+
+    xor eax, eax
+
+
+
+
+
+    mov edx, [ebp + 8] ; todo 为什么是+8？ ;;;;;;;;;;;;;;
+    
+    
+    
+    
+    
+    
+    in al, dx ; 将端口号 dx 的 8 bit 输入到 ax。
+
+    leave ; 恢复栈帧。
     ret
 
 
--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -3,18 +3,19 @@
  * 创建于 2022年7月1日
  */
 
-
-
-char msg[] = "hello os!";
+#include <sys/types.h>
+#include <yros/io.h>
+#include <yros/kernel.h>
+#include <crt.h>
 
 /**
  * 内核初始化。
- * 为防止 C++ 编译器将函数重命名，导致链接失败。
+ * 加入 extern "C" 以防止 C++ 编译器将函数重命名，导致链接失败。
  * 
  */
 extern "C" void kernel_init() {
     char* video = (char*) 0xb8000;
-    for (int i = 0; i < sizeof(msg); i++) {
-        video[i * 2] = msg[i];
-    }
+    video[0]='z';
+    uint8_t data = io_in_byte(CRT_DATA_REG);
+    uint16_t a = 1;
 }
